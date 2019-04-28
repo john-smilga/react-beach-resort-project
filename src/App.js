d6bd65391de39fe02e02d78f536a3f416b0853fa
John <johnsmilga@gmail.com>
2019-04-28 13:44:38 -0700

    exact added to the single room route

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
-        <Route path="/rooms/:slug" component={SingleRoom} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
         <Route component={Error} />
       </Switch>
     </>
